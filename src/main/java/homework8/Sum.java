e54ae9db75fb80f8397c6740981798904cefdd78
inna <innanufal@gmail.com>
2020-12-11 14:07:16 -0500

    HW8

--- src/main/java/homework8/Sum.java
+++ src/main/java/homework8/Sum.java
@@ -3,8 +3,17 @@ public class Sum {
 
 
     public static void main(String[] args) {
-        int[] numbers =  {2,3,5,6,7,8};
-        printSum(numbers);
+        int[] numbers = {2, 3, 5, 6, 7, 8};
+
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(int [] arr){
+        int sum=0;
+        for(int x: arr) {
+            sum = sum +x;
+        }
+        return sum;
 
     }
 
