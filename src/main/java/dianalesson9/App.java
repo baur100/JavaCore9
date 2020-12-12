832ef6f0df14de9b220082190af07182c8b3d791
Diana <kandian1@hotmail.com>
2020-12-12 14:03:28 -0500

    homework8

---
+++ src/main/java/dianalesson9/App.java
@@ -0,0 +1,11 @@
+package dianalesson9;
+
+public class App {
+    public static void main(String[] args) {
+        Person elen = new Person();
+        elen.setName("Elen");
+        String xx = elen.getName();
+        System.out.println(xx);
+    }
+}
+
---
+++ src/main/java/dianalesson9/Person.java
@@ -0,0 +1,15 @@
+package dianalesson9;
+
+public class Person {
+    private String name;
+    private String lastName;
+    private int year;
+
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
