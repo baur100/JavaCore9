57c585650ec2c845ddc1a08bfe17e96b35fa86b7
inna <innanufal@gmail.com>
2020-12-13 11:53:21 -0500

    HW8

---
+++ src/main/java/homework8/AppHome.java
@@ -0,0 +1,29 @@
+package homework8;
+
+public class AppHome{
+    public static void main(String[] args) {
+
+        Home myHouse = new Home();
+        myHouse.streetNumber = 64;
+        myHouse.streetName = "Main";
+        myHouse.aptNumber = 45;
+
+        Home dadHouse = new Home();
+        dadHouse.streetName = "Wyoming ave";
+        dadHouse.streetNumber = 121;
+        dadHouse.aptNumber = 1;
+
+        printHome(streetName);
+        printHome(streetNumber);
+        printHome(aptNumber);
+    }
+
+            public static void printHome(Home home){
+    System.out.println("Steer number = "+home.streetNumber +
+            "\nStreet name = "+home.streetName +
+            "\nApt = "+home.aptNumber );
+
+
+
+    }
+}
