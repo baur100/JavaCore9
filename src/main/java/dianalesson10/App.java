6f07b7e108e5a7a243f5d8fd324fc25737f26efa
Diana <kandian1@hotmail.com>
2020-12-13 23:10:19 -0500

    dianalesson10

---
+++ src/main/java/dianalesson10/App.java
@@ -0,0 +1,7 @@
+package dianalesson10;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/dianalesson10/Person.java
@@ -0,0 +1,45 @@
+package dianalesson10;
+
+public class Person {
+    private String name;
+    private String lastName;
+    private int yearOfBirth;
+
+    public Person(String name, String lastName, int yearOfBirth) {
+        this.name = name;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
