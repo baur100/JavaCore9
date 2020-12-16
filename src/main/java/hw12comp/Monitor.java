b23e4dabb585b9abf11ca20111dd3a7d352e2260
DS19 <dshpakin@gmail.com>
2020-12-15 22:28:54 -0800

    Lesson 12 homeWork

---
+++ src/main/java/hw12comp/Monitor.java
@@ -0,0 +1,36 @@
+package hw12comp;
+
+public class Monitor {
+
+    private int diagonal;
+    private String brand;
+
+    public Monitor(int diagonal, String brand) {
+        this.diagonal = diagonal;
+        this.brand = brand;
+    }
+
+    public int getDiagonal() {
+        return diagonal;
+    }
+
+    public void setDiagonal(int diagonal) {
+        this.diagonal = diagonal;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor{" +
+                "diagonal=" + diagonal +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+}
