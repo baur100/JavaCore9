1383b8b031db258cfec80ed63b86643d66fa36f8
inna <Innamammadova584@gmail.com>
2020-12-18 00:34:14 -0600

    Homework10-11

---
+++ src/main/java/homework11/HW11.java
@@ -0,0 +1,16 @@
+package homework11;
+
+public class HW11 {
+    public static void main(String[] args) {
+        Building building = Building.HOUSE;
+        String buildingBuilding = "Home";
+//        System.out.println(buildingBuilding);
+
+    }
+    Colors [] colors= {Colors.WHITE, Colors.BLACK, Colors.SILVER,Colors.GRAPHITE };
+
+    Vehicle volvo= new Vehicle("Volvo","XC90", 2018, colors);
+    
+
+
+}
