f79c9e16d71040e44d4a7e5cfc5523c9d3e5671c
orloffny <astanislau@gmail.com>
2020-12-23 18:17:34 -0500

    HomeWork for lessons 16 - Hospital - Revision 2

--- src/main/java/homeWork16SO03/App.java
+++ src/main/java/homeWork16SO03/App.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-       Hospital princetonPlainsboro = new Hospital("Princeton-Plainsboro Teaching Hospital",new Address("1 Plainsboro Rd","Princeton","NJ",8536));
+
        Doctor house = new Doctor("Gregory","House",Position.NEPHROLOGIST);
        Doctor wilson = new Doctor("James","Wilson",Position.ONCOLOGIST);
 
@@ -16,21 +16,24 @@ public class App {
        doctors.add(wilson);
 
 
-        HashMap<Integer,String > rooms = new HashMap<>();
-        rooms.put(1, "Exam room");
-        rooms.put(4, "Ophthalmology");
-        rooms.put(23, "Oncology");
+        HashMap<Integer, Position > rooms = new HashMap<>();
+        rooms.put(1, Position.ALLERGIST);
+        rooms.put(4, Position.ENDOCRINOLOGIST);
+        rooms.put(23, Position.NEPHROLOGIST);
 
         ArrayList<InsuranceCompanies> acceptedInsurances = new ArrayList<>();
         acceptedInsurances.add(InsuranceCompanies.AETNA);
         acceptedInsurances.add(InsuranceCompanies.UNITEDHEALTHCARE);
         acceptedInsurances.add(InsuranceCompanies.GUARDIAN);
 
-        System.out.println(princetonPlainsboro);
         System.out.println(doctors);
         System.out.println(rooms);
 
+        Address address = new Address("1 Plainsboro Rd","Princeton","NJ",8536);
+        Hospital princetonPlainsboro = new Hospital("Princeton-Plainsboro Teaching Hospital",address ,doctors, acceptedInsurances, rooms);
+
         System.out.println(acceptedInsurances);
+        princetonPlainsboro.printHospitalInfo();
 
     }
 }
--- src/main/java/homeWork16SO03/Hospital.java
+++ src/main/java/homeWork16SO03/Hospital.java
@@ -1,32 +1,36 @@
 package homeWork16SO03;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Hospital {
     private String hospitalName;
     private Address address;
+    private List<Doctor> doctors;
+    private List<InsuranceCompanies> companies;
+    private HashMap<Integer, Position> rooms;
 
-    public Hospital(String hospitalName, Address address) {
+
+    public Hospital(String hospitalName, Address address, List<Doctor> doctors, ArrayList<InsuranceCompanies> companies, HashMap<Integer, Position> rooms) {
         this.hospitalName = hospitalName;
         this.address = address;
+        this.doctors = doctors;
+        this.companies = companies;
+        this.rooms = rooms;
     }
 
-    public String getHospitalName() {
-        return hospitalName;
-    }
 
-    public void setHospitalName(String hospitalName) {
-        this.hospitalName = hospitalName;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return hospitalName + " " + address;
+    public void printHospitalInfo(){
+        System.out.println("Hospital name: " + hospitalName + ", address = " + address);
+        for (Doctor d: doctors){
+            System.out.println(d);
+        }
+        for (InsuranceCompanies i: companies){
+            System.out.println(i);
+        }
+        rooms.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        });
     }
 }
