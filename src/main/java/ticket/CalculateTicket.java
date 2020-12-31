1de82299efcbf5c213172bf29ed5813ca05250ba
baur <baurzhan.zh@gmail.com>
2020-12-30 22:55:34 -0500

    Lesson 4 - TestNG - Unit testing

---
+++ src/main/java/ticket/CalculateTicket.java
@@ -0,0 +1,23 @@
+package ticket;
+
+public class CalculateTicket {
+    private double baseFare;
+    private int age;
+
+    public CalculateTicket(double baseFare, int age) {
+        this.baseFare = baseFare;
+        this.age = age;
+    }
+    public double getPrice(){
+        if(this.age<=2){
+            return 0;
+        }
+        if (this.age<=12){
+            return this.baseFare*0.5;
+        }
+        if(this.age>66){
+            return this.baseFare*0.8;
+        }
+        return this.baseFare;
+    }
+}
