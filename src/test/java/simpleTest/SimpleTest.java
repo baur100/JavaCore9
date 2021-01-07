86d9f8e0085b0df816b8a77f0f21979e2e3d6174
baur <baurzhan.zh@gmail.com>
2021-01-06 22:49:32 -0500

    Lesson 6 - TestNG + Selenium

--- src/test/java/simpleTest/SimpleTest.java
+++ src/test/java/simpleTest/SimpleTest.java
@@ -6,24 +6,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class SimpleTest {
-    @Test
-    public void loginTest_correctCredentials_loggedToApp() throws InterruptedException {
+    WebDriver driver;
+    @BeforeMethod
+    public void startUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.get("https://koelapp.testpro.io/");
         Thread.sleep(1000);
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.close();
+    }
+    @Test
+    public void loginTest_correctCredentials_loggedToApp() throws InterruptedException {
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
         WebElement passwordField = driver.findElement(By.xpath("//*[@type='password']"));
         WebElement loginButton = driver.findElement(By.cssSelector("button"));
 
         emailField.sendKeys("koeluser06@testpro.io");
-        passwordField.sendKeys("te$t$tudent"); // "WrongPassword"
+        passwordField.sendKeys("te$t$tudent1"); // "WrongPassword"
         loginButton.click();
 
         Thread.sleep(1500);
@@ -39,13 +50,21 @@ public class SimpleTest {
 
         List<WebElement> homes = driver.findElements(By.xpath("//*[@class='home active']"));
         Assert.assertEquals(1,homes.size());
-
-        Thread.sleep(3000);
-        driver.close();
     }
 
     @Test
-    public void loginTest_wrongCredentials_errorFrame(){
+    public void loginTest_wrongCredentials_errorFrame() throws InterruptedException {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement passwordField = driver.findElement(By.xpath("//*[@type='password']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button"));
 
+        emailField.sendKeys("koeluser06@testpro.io");
+        passwordField.sendKeys("WrongPassword");
+        loginButton.click();
+
+        Thread.sleep(1500);
+
+        List<WebElement> homes = driver.findElements(By.cssSelector(".e1rror"));
+        Assert.assertEquals(1,homes.size());
     }
 }
