a88bac587b7e1390c9ebba31a70d253e62297ad9
Baurzhan Dosym <baurzhan.zh@gmail.com>
2021-02-06 15:27:24 -0500

    Lesson 9-10 - Koel API tests

---
+++ src/test/java/koelTests/PlaylistTests.java
@@ -0,0 +1,116 @@
+package koelTests;
+
+import com.github.javafaker.Faker;
+import helpers.Token;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import models.CreatePlaylistRequest;
+import models.CreatePlaylistResponse;
+import models.RenamePlaylistRequest;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PlaylistTests {
+    private Faker faker;
+    private int playlistId;
+    private String playlistName;
+    private String token;
+    @BeforeClass
+    public void runOnce(){
+         token = Token.get();
+    }
+    @BeforeMethod
+    public void startUp(){
+        faker = new Faker();
+
+        playlistName = faker.artist().name();
+        CreatePlaylistRequest createPlaylistRequest = new CreatePlaylistRequest(playlistName);
+        Response response =
+                given()
+                        .baseUri("https://koelapp.testpro.io/")
+                        .basePath("api/playlist")
+                        .header("Content-Type","application/json")
+                        .header("Accept", "application/json")
+                        .header("Authorization",token)
+                        .body(createPlaylistRequest)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        JsonPath jsonPath = response.jsonPath();
+        CreatePlaylistResponse createPlaylistResponse = jsonPath.getObject("$",CreatePlaylistResponse.class);
+        playlistId = createPlaylistResponse.getId();
+        Assert.assertEquals(playlistName,createPlaylistResponse.getName());
+        System.out.println(playlistId);
+    }
+    @AfterMethod
+    public void tearDown(){
+        given()
+                .baseUri("https://koelapp.testpro.io/")
+                .basePath("api/playlist/"+playlistId)
+                .header("Authorization",token)
+                .when()
+                .delete()
+                .then()
+                .extract()
+                .response();
+    }
+    @Test
+    public void getPlaylist(){
+
+        Response response =
+                given()
+                        .baseUri("https://koelapp.testpro.io/")
+                        .basePath("api/playlist")
+                        .header("Content-Type","application/json")
+                        .header("Accept", "application/json")
+                        .header("Authorization",token)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        JsonPath jsonPath = response.jsonPath();
+        CreatePlaylistResponse[] createPlaylists = jsonPath.getObject("$",CreatePlaylistResponse[].class);
+        boolean found = false;
+        for (CreatePlaylistResponse playlist :  createPlaylists){
+            if(playlist.getId()==playlistId){
+                Assert.assertEquals(playlist.getName(),playlistName);
+                found=true;
+                break;
+            }
+        }
+        Assert.assertTrue(found);
+    }
+    @Test
+    public void renamePlaylist(){
+        String updatedName = faker.funnyName().name();
+        RenamePlaylistRequest renamePlaylistRequest = new RenamePlaylistRequest(updatedName);
+        Response response =
+                given()
+                        .baseUri("https://koelapp.testpro.io/")
+                        .basePath("api/playlist/"+playlistId)
+                        .header("Content-Type","application/json")
+                        .header("Accept", "application/json")
+                        .header("Authorization",token)
+                        .body(renamePlaylistRequest)
+                        .when()
+                        .patch()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        JsonPath jsonPath = response.jsonPath();
+        CreatePlaylistResponse createPlaylistResponse = jsonPath.getObject("$",CreatePlaylistResponse.class);
+        Assert.assertEquals(createPlaylistResponse.getName(),updatedName);
+
+    }
+}
