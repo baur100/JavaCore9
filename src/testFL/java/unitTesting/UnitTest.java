a17102581c11be7195abce40f632c547135f1d9e
Irina <irina19933@gmail.com>
2021-07-16 20:50:30 -0400

    zFLselenium - lesson 4 - Unit tests

---
+++ src/testFL/java/unitTesting/UnitTest.java
@@ -0,0 +1,37 @@
+package unitTesting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import zFLticket.CalculateTicket;
+
+public class UnitTest {
+    @Test
+    public void ticketTests_infant_ticketPriceIsZero(){
+        // Arrange (given)
+        CalculateTicket infant = new CalculateTicket(1000, 1);
+
+        // Act (when)
+        double price = infant.getPrice();
+
+        //Assert (then)
+        Assert.assertEquals(price,0.0);
+    }
+    @Test
+    public void ticketTests_child_ticketPriceIs50percent(){
+        CalculateTicket child = new CalculateTicket(1000,5);
+        double price = child.getPrice();
+        Assert.assertEquals(price,500.0);
+    }
+    @Test
+    public void ticketTests_adult_ticketPriceIsFull(){
+        CalculateTicket adult = new CalculateTicket(1000,25);
+        double price = adult.getPrice();
+        Assert.assertEquals(price,1000.0);
+    }
+    @Test
+    public void ticketTests_senior_ticketPriceIs80percent(){
+        CalculateTicket senior = new CalculateTicket(1000,70);
+        double price = senior.getPrice();
+        Assert.assertEquals(price, 800.0);
+    }
+}
